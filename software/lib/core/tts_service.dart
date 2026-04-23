f76af96a9a56873623cc9e1cfea6688fe34cfc82
Hà Đức Thuận <haducthuan2003cpqn@gmail.com>
2026-04-23 12:00:14 +0700

    fix init tts

--- software/lib/core/tts_service.dart
+++ software/lib/core/tts_service.dart
@@ -1,19 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 class TtsService {
   final _tts = FlutterTts();
+  bool _isReady = false;
 
   Future<void> init() async {
-    await _tts.setLanguage('vi-VN');
-    await _tts.setSpeechRate(0.45);
-    await _tts.setVolume(1.0);
-    await _tts.setPitch(1.0);
+    try {
+      var langs = await _tts.getLanguages;
+
+      debugPrint("Available TTS languages: $langs");
+
+      if (langs.contains("vi-VN")) {
+        await _tts.setLanguage("vi-VN");
+      } else {
+        debugPrint("vi-VN not found → fallback en-US");
+        await _tts.setLanguage("en-US");
+      }
+
+      await _tts.setSpeechRate(0.45);
+      await _tts.setVolume(1.0);
+      await _tts.setPitch(1.0);
+
+      _isReady = true;
+    } catch (e) {
+      debugPrint("TTS init error: $e");
+      _isReady = false;
+    }
   }
 
   Future<void> speak(String text) async {
-    if (text.isEmpty) return;
-    await _tts.stop();
-    await _tts.speak(text);
+    if (!_isReady || text.isEmpty) return;
+
+    try {
+      await _tts.stop();
+      await _tts.speak(text);
+    } catch (e) {
+      debugPrint("TTS speak error: $e");
+    }
   }
 
   Future<void> stop() async => _tts.stop();
